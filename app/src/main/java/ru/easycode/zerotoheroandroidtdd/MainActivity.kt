7d6af726f5d7d082c8e59168bde1039aef53c3b5
Egor <dbn.orlov@gmail.com>
2025-06-02 15:53:43 +0300

    task 6 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,22 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.changeButton.setOnClickListener { binding.titleTextView.text =
+            getString(R.string.i_am_an_android_developer) }
     }
 }
