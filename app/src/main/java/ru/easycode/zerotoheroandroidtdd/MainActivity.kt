275ede38aa45ad92810e2b153b6587125aa8d2ae
Egor <dbn.orlov@gmail.com>
2025-06-02 22:19:21 +0300

    task 7 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
+
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        if (savedInstanceState != null) {
+            binding.titleTextView.text = savedInstanceState.getString(KEY).toString()
+        }
 
-        binding.changeButton.setOnClickListener { binding.titleTextView.text =
-            getString(R.string.i_am_an_android_developer) }
+
+        binding.changeButton.setOnClickListener {
+            binding.titleTextView.text =
+                getString(R.string.i_am_an_android_developera)
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key",getString(R.string.i_am_an_android_developera))
+    }
+
+    companion object {
+        const val KEY = "key"
+    }
+
+}
