cc58ab9907d8934231f405fc93040d80e2e9edaf
Egor <dbn.orlov@gmail.com>
2025-06-03 00:28:30 +0300

    task 9 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,47 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        savedInstanceState?.let {
+            if (!savedInstanceState.getBoolean(KEYTEXT)){
+                binding.rootLayout.removeView(binding.titleTextView)
+            }
+            binding.removeButton.isEnabled = savedInstanceState.getBoolean(KEYBUTTON)
+        }
+
+        binding.removeButton.setOnClickListener {
+            binding.rootLayout.removeView(binding.titleTextView)
+            it.isEnabled = false
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putBoolean(KEYBUTTON, binding.removeButton.isEnabled)
+        outState.putBoolean(KEYTEXT, binding.titleTextView.isAttachedToWindow)
+    }
+
+    companion object {
+        const val KEYBUTTON = "keyButton"
+        const val KEYTEXT = "keyText"
     }
 }
